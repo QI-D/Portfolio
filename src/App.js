b385e37845e991368b244d1a07c296bad15e3c1b
QI-D <jonsondang@outlook.com>
2021-04-23 11:04:25 -0700

    nav bar using react

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Qi from './Images/Qi.JPG';
 import './App.css';
 import Typical from 'react-typical';
 
-
-
 function App() {
   return (
     <div className="App">
+      <ul class="nav">
+      <li><a href="/">Home</a></li>
+      <li><a href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</a></li>
+      <li><a href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+      <li><a href="/projects.html">Projects</a></li>
+      <li><a href="/Qi_Dang_Resume.pdf" target="_blank">Resume</a></li>
+    </ul>
+
       <header className="App-header">
         <img src={Qi} className="App-logo" alt="logo" />
 
