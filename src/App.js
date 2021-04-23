7db4f9f49cbfb533be1ef81ea8d2cabad1fff5fa
QI-D <jonsondang@outlook.com>
2021-04-23 14:42:32 -0700

    navbar using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React from 'react';
 import Qi from './Images/Qi.JPG';
 import './App.css';
 import Typical from 'react-typical';
+import * as ReactBootStrap from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <ul class="nav">
-      <li><a href="/">Home</a></li>
-      <li><a href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</a></li>
-      <li><a href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-      <li><a href="/projects.html">Projects</a></li>
-      <li><a href="/Qi_Dang_Resume.pdf" target="_blank">Resume</a></li>
-    </ul>
+    <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
+      <ReactBootStrap.Navbar.Brand id="home" href="/">QD</ReactBootStrap.Navbar.Brand>
+      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto nav-options">
+          {/* <ReactBootStrap.Nav.Link href="/projects">Projects</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/Qi_Dang_Resume.pdf" target="_blank">Resume</ReactBootStrap.Nav.Link> */}
+          {/* <ReactBootStrap.NavDropdown title="More" id="collasible-nav-dropdown">
+            <ReactBootStrap.NavDropdown.Item href="/Qi_Dang_Resume.pdf" target="_blank">Resume</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Divider />
+            <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+          </ReactBootStrap.NavDropdown> */}
+        </ReactBootStrap.Nav>
+        <ReactBootStrap.Nav>
+          {/* <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </ReactBootStrap.Nav.Link> */}
+          <ReactBootStrap.Nav.Link href="/projects">Projects</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/Qi_Dang_Resume.pdf" target="_blank">Resume</ReactBootStrap.Nav.Link>
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
 
       <header className="App-header">
         <img src={Qi} className="App-logo" alt="logo" />
