59278666bccb8227141953b7c3f45797c24cf49f
QI-D <jonsondang@outlook.com>
2021-06-05 16:26:48 -0700

    Updated title, resume

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import * as ReactBootStrap from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-    <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
-      <ReactBootStrap.Navbar.Brand id="home" href="/home">QD</ReactBootStrap.Navbar.Brand>
-      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-        <ReactBootStrap.Nav className="mr-auto">
+      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
+        <ReactBootStrap.Navbar.Brand id="home" href="/home">QD</ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
 
-        </ReactBootStrap.Nav>
-        <ReactBootStrap.Nav>
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav>
 
-          {/* <ReactBootStrap.Nav.Link href="/projects">Projects</ReactBootStrap.Nav.Link> */}
-          <ReactBootStrap.Nav.Link href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="/Qi_Dang_Resume.pdf" target="_blank">Resume</ReactBootStrap.Nav.Link>
-        </ReactBootStrap.Nav>
-      </ReactBootStrap.Navbar.Collapse>
+            {/* <ReactBootStrap.Nav.Link href="/projects">Projects</ReactBootStrap.Nav.Link> */}
+            <ReactBootStrap.Nav.Link href="https://github.com/QI-D" target="_blank" rel="noreferrer">GitHub</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="https://www.linkedin.com/in/qi-dang-84594311b/" target="_blank" rel="noreferrer">LinkedIn</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="/Qi_Dang_Resume.pdf" target="_blank">Resume</ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
       </ReactBootStrap.Navbar>
 
       <header className="App-header">
@@ -29,7 +29,7 @@ function App() {
 
         <h1>Hi, I am Qi Dang </h1>
         <p>I am a {' '}
-          <Typical 
+          <Typical
             loop={Infinity}
             wrapper="b"
             steps={[
@@ -37,7 +37,7 @@ function App() {
               1500,
               'Full Stack Developer',
               1500,
-              'System Engineer',
+              'Systems Engineer',
               1500
             ]}
           />
